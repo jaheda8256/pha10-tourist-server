b7df24c234e0be36ce0ddfaa0bc0ba3daa2e7cdc
jaheda8256 <jahedasultana47@gmail.com>
2024-04-27 19:44:00 +0600

    added new tourists page

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,8 +27,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+  
     const touristCollection = client.db('touristsDB').collection('tourists');
+
+
+
+    app.get('/tourists', async(req, res) =>{
+        const cursor = touristCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
 
     app.post('/tourists', async(req, res)=>{
         const addTourists = req.body;
@@ -39,9 +47,9 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+   
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deployment. You successfully connected to MongoDB!!"
     );
   } finally {
     // Ensures that the client will close when you finish/error

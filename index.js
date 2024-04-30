b9233ef6bcc01de6745df31bd43ff83dd1a472fa
jaheda8256 <jahedasultana47@gmail.com>
2024-04-30 10:25:33 +0600

    added put

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wotzmkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,53 +25,99 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-  
-    const touristCollection = client.db('touristsDB').collection('tourists');
-   
 
+    const touristCollection = client.db("touristsDB").collection("tourists");
 
-    app.get('/tourists', async(req, res) =>{
-        const cursor = touristCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    // my list
+    app.get("/tourists", async (req, res) => {
+      const cursor = touristCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-    app.post('/tourists', async(req, res)=>{
-        const addTourists = req.body;
-        console.log(addTourists);
-        const result = await touristCollection.insertOne(addTourists);
-        res.send(result);
-    })
-
-
+    // update
 
     app.get("/tourists/:id", async (req, res) => {
-        try {
-          const id = req.params.id;
-          console.log(id);
-          const result = await touristCollection.findOne({ _id: new ObjectId(id) });
-          if (result) {
-            res.send(result);
-          } else {
-            res.status(404).json({ error: "Tourists not found" });
-          }
-        } catch (error) {
-          console.error("Error fetching tourists:", error);
-          res.status(500).json({ error: "Internal server error" });
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update put
+    app.put("/tourists/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedTourists = req.body;
+      
+      const tourists = {
+        $set: {
+          name: updatedTourists.name,
+          cost: updatedTourists.cost,
+          seasonality: updatedTourists.seasonality,
+          travel: updatedTourists.travel,
+          totalVisitorsPerYear: updatedTourists.totalVisitorsPerYear,
+          photo: updatedTourists.photo,
+          country: updatedTourists.country,
+          location: updatedTourists.location,
+          description: updatedTourists.description,
+        },
+      };
+      const result = await touristCollection.updateOne(filter, tourists, options);
+      res.send(result);
+    });
+
+    // my list delete
+    app.delete("/tourists/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post("/tourists", async (req, res) => {
+      const addTourists = req.body;
+      console.log(addTourists);
+      const result = await touristCollection.insertOne(addTourists);
+      res.send(result);
+    });
+
+    app.get("/tourists/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id);
+        const result = await touristCollection.findOne({
+          _id: new ObjectId(id),
+        });
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).json({ error: "Tourists not found" });
         }
-      });
+      } catch (error) {
+        console.error("Error fetching tourists:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
 
     //   user related apis
-    app.get('/tourists/:email', async (req, res) => {
-        const query = { userEmail: req.params.email }
-        const cursor =touristCollection.find(query)
-        const data = await cursor.toArray()
-        res.send(data)
-      })
+    app.get("/tourists-email/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const cursor = touristCollection.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
+
+    app.get("/tourists/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-   
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!!"
     );
